c4035a4f0ab0ba6f909390cbd9181af3b49352c1
diogo-martins-wingman <43008015+diogo-martins-wingman@users.noreply.github.com>
2018-09-05 17:16:58 +0100

    Add files via upload

--- www/entelplugin.js
+++ www/entelplugin.js
@@ -3,7 +3,7 @@ function EntelPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+EntelPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
