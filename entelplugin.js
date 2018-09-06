afb07560b9fee251df70738f70e71123a5461dd7
diogo-martins-wingman <43008015+diogo-martins-wingman@users.noreply.github.com>
2018-09-06 15:58:34 +0100

    Add files via upload

---
+++ entelplugin.js
@@ -0,0 +1,21 @@
+// Empty constructor
+function EntelPlugin() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+EntelPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'EntelPlugin', 'show', [options]);
+}
+
+// Installation constructor that binds ToastyPlugin to window
+EntelPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.entelPlugin = new EntelPlugin();
+  return window.plugins.entelPlugin;
+};
+cordova.addConstructor(EntelPlugin.install);
