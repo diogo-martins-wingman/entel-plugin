5372771313572541f0dad3dd0b4389f824573b32
diogo-martins-wingman <43008015+diogo-martins-wingman@users.noreply.github.com>
2018-09-07 11:51:17 +0100

    Update EntelPlugin.java

--- src/android/EntelPlugin.java
+++ src/android/EntelPlugin.java
@@ -1,50 +1,28 @@
 package pt.wingman.entel.plugin;
-// The native Toast API
+
 import android.content.Context;
 import android.widget.Toast;
-// Cordova-required packages
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import pt.wingman.entel.plugin.Teste;
 import pt.wingman.entel.plugin.FingerprintManager;
 import pt.wingman.entel.plugin.FingerprintManagerCallback;
 
 public class EntelPlugin extends CordovaPlugin {
-  private static final String DURATION_LONG = "long";
   @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-      //Toast.makeText(cordova.getActivity(), "dsa", Toast.LENGTH_LONG).show();
-      //Teste.teste(cordova.getActivity().getApplicationContext());
-      Context context = cordova.getActivity().getApplicationContext();
-      FingerprintManager.getInstance().initialize(context,getFingerprintManagerCallback(context), 1, 0, false);
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
+        if(action.equals("start")) {
+            Context context = cordova.getActivity().getApplicationContext();
+            FingerprintManager.getInstance().initialize(context, getFingerprintManagerCallback(context), 1, 0, false);
+        }
 
-      /*if (!action.equals("show")) {
-          System.out.println("teste");
-        callbackContext.error("\"" + action + "\" is not a recognized action.");
-        return false;
-      }
-      String message;
-      String duration;
-      try {
-        JSONObject options = args.getJSONObject(0);
-        message = options.getString("message");
-        duration = options.getString("duration");
-      } catch (JSONException e) {
-        callbackContext.error("Error encountered: " + e.getMessage());
-        return false;
-      }
-      // Create the toast
-      Toast toast = Toast.makeText(cordova.getActivity(), message,
-        DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-      // Display toast
-      toast.show();
-      // Send a positive result to the callbackContext*/
+        if((!action.equals("stop"))){
+            FingerprintManager.getInstance().stop()
+        }
+
       PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
       callbackContext.sendPluginResult(pluginResult);
       return true;
