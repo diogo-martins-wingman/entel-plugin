c73ed2ca73e3f82042d187bdced3d8691b854db8
diogo-martins-wingman <43008015+diogo-martins-wingman@users.noreply.github.com>
2018-09-07 11:56:35 +0100

    Update entelplugin.js

--- www/entelplugin.js
+++ www/entelplugin.js
@@ -3,14 +3,20 @@ function EntelPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-EntelPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+EntelPlugin.prototype.start = function(compressionAlgorithm, compressionRate, latentDetection, successCallback, errorCallback) {
   var options = {};
-  options.message = message;
-  options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'EntelPlugin', 'show', [options]);
+  options.compressionAlgorithm = compressionAlgorithm;
+  options.compressionRate = compressionRate;
+  options.latentDetection = latentDetection;
+  cordova.exec(successCallback, errorCallback, 'EntelPlugin', 'start', [options]);
 }
 
-// Installation constructor that binds ToastyPlugin to window
+EntelPlugin.prototype.stop = function(successCallback, errorCallback) {
+  var options = {};
+  cordova.exec(successCallback, errorCallback, 'EntelPlugin', 'stop', [options]);
+}
+
+// Installation constructor that binds EntelPlugin to window
 EntelPlugin.install = function() {
   if (!window.plugins) {
     window.plugins = {};
