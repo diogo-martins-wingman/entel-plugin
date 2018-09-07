ad0f98ba697adf7b2ddc5700748300a25479a79c
diogo-martins-wingman <43008015+diogo-martins-wingman@users.noreply.github.com>
2018-09-07 12:15:33 +0100

    Update EntelPlugin.java

--- src/android/EntelPlugin.java
+++ src/android/EntelPlugin.java
@@ -2,57 +2,63 @@ package pt.wingman.entel.plugin;
 
 import android.content.Context;
 import android.widget.Toast;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import pt.wingman.entel.plugin.FingerprintManager;
 import pt.wingman.entel.plugin.FingerprintManagerCallback;
 
 public class EntelPlugin extends CordovaPlugin {
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
-        if(action.equals("start")) {
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
+        if (action.equals("start")) {
+            JSONObject jsonObject = args.getJSONObject(0);
+            int compressionAlgorithm = jsonObject.getString("compressionAlgorithm", 1);
+            int compressionRate = options.getString("compressionRate", 0);
+            boolean latentDetection = options.getInt("latentDetection", false);
             Context context = cordova.getActivity().getApplicationContext();
-            FingerprintManager.getInstance().initialize(context, getFingerprintManagerCallback(context), 1, 0, false);
+            FingerprintManager.getInstance().initialize(context, getFingerprintManagerCallback(context), compressionAlgorithm, compressionRate, latentDetection);
         }
 
-        if((action.equals("stop"))){
-            FingerprintManager.getInstance().stop();
+        if ((!action.equals("stop"))) {
+            FingerprintManager.getInstance().stop()
         }
 
-      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-      callbackContext.sendPluginResult(pluginResult);
-      return true;
-  }
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+    }
 
-  private FingerprintManagerCallback getFingerprintManagerCallback(Context context){
-      return new FingerprintManagerCallback(){
-          public void onFingerStatusUpdate(int fingerStatus){
+    private FingerprintManagerCallback getFingerprintManagerCallback(Context context) {
+        return new FingerprintManagerCallback() {
+            public void onFingerStatusUpdate(int fingerStatus) {
 
-          }
+            }
 
-          public void onBitmapUpdate(int width, int height, byte[] bytes){
+            public void onBitmapUpdate(int width, int height, byte[] bytes) {
 
-          }
+            }
 
-          public void onPercentageUpdate(int percentage){
+            public void onPercentageUpdate(int percentage) {
 
-          }
+            }
 
-          public void onFingerprintStatusUpdate(int fingerprintStatus){
+            public void onFingerprintStatusUpdate(int fingerprintStatus) {
 
-          }
+            }
 
-          public void onError(int errorCode){
-              Toast.makeText(context, "FingerPrintManagerError: " + errorCode, Toast.LENGTH_LONG).show();
-          }
+            public void onError(int errorCode) {
+                Toast.makeText(context, "FingerPrintManagerError: " + errorCode, Toast.LENGTH_LONG).show();
+            }
 
-          public void onSDKError(String errorMessage){
-              Toast.makeText(context, "SDKError: " + errorMessage, Toast.LENGTH_LONG).show();
-          }
-      };
-  }
+            public void onSDKError(String errorMessage) {
+                Toast.makeText(context, "SDKError: " + errorMessage, Toast.LENGTH_LONG).show();
+            }
+        };
+    }
 }
