e44287deba075b75cb61384bce3bd0f17a1f1967
diogo-martins-wingman <43008015+diogo-martins-wingman@users.noreply.github.com>
2018-09-10 12:38:16 +0100

    Update EntelPlugin.java

--- src/android/EntelPlugin.java
+++ src/android/EntelPlugin.java
@@ -20,24 +20,28 @@ public class EntelPlugin extends CordovaPlugin {
             int compressionAlgorithm;
             int compressionRate;
             boolean latentDetection;
-            
+
             try {
                 JSONObject jsonObject = args.getJSONObject(0);
                 compressionAlgorithm = jsonObject.getInt("compressionAlgorithm");
                 compressionRate = jsonObject.getInt("compressionRate");
                 latentDetection = jsonObject.getBoolean("latentDetection");
             } catch (JSONException e) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.JSON_EXCEPTION);
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.JSON_EXCEPTION);
                 callbackContext.error(e.getMessage());
                 return false;
             }
-         
-            Context context = cordova.getActivity().getApplicationContext();
-            FingerprintManager.getInstance().initialize(context, getFingerprintManagerCallback(context, callbackContext), compressionAlgorithm, compressionRate, latentDetection);
 
-            PluginResult pluginResult = new  PluginResult(PluginResult.Status.NO_RESULT);
-            pluginResult.setKeepCallback(true);
-            callbackContext.sendPluginResult(pluginResult);
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    Context context = cordova.getActivity().getApplicationContext();
+                    FingerprintManager.getInstance().initialize(context, getFingerprintManagerCallback(context, callbackContext), compressionAlgorithm, compressionRate, latentDetection);
+                    PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+                    pluginResult.setKeepCallback(true);
+                    callbackContext.sendPluginResult(pluginResult);
+                }
+            });
             return true;
         }
 
@@ -56,37 +60,37 @@ public class EntelPlugin extends CordovaPlugin {
     private FingerprintManagerCallback getFingerprintManagerCallback(Context context, CallbackContext callbackContext) {
         return new FingerprintManagerCallback() {
             public void onFingerStatusUpdate(int fingerStatus) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.OK, "{\"onFingerStatusUpdate\":{\"fingerStatus\":"+fingerStatus+"}}");
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "{\"onFingerStatusUpdate\":{\"fingerStatus\":" + fingerStatus + "}}");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
             }
 
             public void onBitmapUpdate(int width, int height, String base64String) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.OK, "{\"onBitmapUpdate\":{\"width\":"+width+",\"height\":"+height+",\"base64String\":\""+base64String+"\"}}");
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "{\"onBitmapUpdate\":{\"width\":" + width + ",\"height\":" + height + ",\"base64String\":\"" + base64String + "\"}}");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
             }
 
             public void onPercentageUpdate(int percentage) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.OK, "{\"onPercentageUpdate\":{\"percentage\":"+percentage+"}}");
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "{\"onPercentageUpdate\":{\"percentage\":" + percentage + "}}");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
             }
 
             public void onFingerprintStatusUpdate(int fingerprintStatus) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.OK, "{\"onFingerprintStatusUpdate\":{\"fingerprintStatus\":"+fingerprintStatus+"}}");
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "{\"onFingerprintStatusUpdate\":{\"fingerprintStatus\":" + fingerprintStatus + "}}");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
             }
 
             public void onError(int errorCode) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.ERROR, "{\"onError\":{\"errorCode\":"+errorCode+"}}");
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "{\"onError\":{\"errorCode\":" + errorCode + "}}");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
             }
 
             public void onSDKError(int sdkErrorCode, String errorMessage) {
-                PluginResult pluginResult = new  PluginResult(PluginResult.Status.ERROR, "{\"onSDKError\":{\"sdkErrorCode\":"+sdkErrorCode+",\"errorMessage\":\""+errorMessage+"\"}}");
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "{\"onSDKError\":{\"sdkErrorCode\":" + sdkErrorCode + ",\"errorMessage\":\"" + errorMessage + "\"}}");
                 pluginResult.setKeepCallback(true);
                 callbackContext.sendPluginResult(pluginResult);
             }
